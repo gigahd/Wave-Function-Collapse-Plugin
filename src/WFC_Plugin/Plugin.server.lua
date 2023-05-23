9cd4e8a42525f9fa03157757fc3bfed5bf6c5a74
gigahd <benaltmann2004@gmail.com>
2023-05-23 04:59:26 +0200

    Added Tile adding and Connection Changes

--- src/WFC_Plugin/Plugin.server.lua
+++ src/WFC_Plugin/Plugin.server.lua
@@ -1,3 +1,4 @@
+local ServerStorage = game:GetService("ServerStorage")
 local Studio = settings():GetService("Studio")
 local Selection = game:GetService("Selection")
 
@@ -13,8 +14,19 @@ local AddTileButton = TileAdderFrame:WaitForChild("GenerateFrame"):WaitForChild(
 local TileNameBox = TileAdderFrame:WaitForChild("TileName")
 local TileModelFrame = TileAdderFrame:WaitForChild("ModelFrame")
 local TileConectionsFrame = TileAdderFrame:WaitForChild("Connections")
+local TileAdderCloseButton = TileAdderFrame:WaitForChild("CloseButton")
 
 local TileModelButton = TileModelFrame:WaitForChild("Button")
+
+local TilesListFrame = MainFrame:WaitForChild("TilesList")
+
+local TilesList = TilesListFrame:WaitForChild("List")
+local TemplateTile = TilesListFrame:WaitForChild("TileTemplate")
+
+local TileConnectionsFrame: Frame = MainFrame:WaitForChild("ConnectionsFrame")
+
+local TileConnectionsList: Frame = TileConnectionsFrame:WaitForChild("List")
+local TileConnectionsCloseButton: TextButton = TileConnectionsFrame:WaitForChild("CloseButton")
 
 local pluginButton = toolbar:CreateButton("WFC", "Generate Grids using WFC", "rbxassetid://6811954238")
 
@@ -35,6 +47,12 @@ local info = DockWidgetPluginGuiInfo.new(
 
 local tileAdderIsOpen = false
 local modelSelectionMode = false
+
+local tileConnectionsIsOpen = false
+local tileConnected = nil
+
+
+TileModelFrame.CurrentCamera = TileModelFrame.Camera
 
 local widget = plugin:CreateDockWidgetPluginGui(
 "TestPlugin", --A unique and consistent identifier used to storing the widget’s dock state and other internal details
@@ -58,6 +76,15 @@ local function changeButtonColor(value: GuiBase)
 	value.BackgroundColor3 = Theme:GetColor(Enum.StudioStyleGuideColor.MainButton)
 end
 
+local function changeToPointIconOnHover(value: GuiBase)
+	value.MouseEnter:Connect(function()
+		mouse.Icon = pointCursor
+	end)
+	value.MouseLeave:Connect(function()
+		mouse.Icon = normalCursor
+	end)
+end
+
 local function changeColor()
 	changeBackGroundColor(MainFrame)
 	
@@ -69,12 +96,7 @@ local function changeColor()
 		elseif value:IsA("TextButton") then
 			changeTextColor(value)
 			changeButtonColor(value)
-			value.MouseEnter:Connect(function()
-				mouse.Icon = pointCursor
-			end)
-			value.MouseLeave:Connect(function()
-				mouse.Icon = normalCursor
-			end)
+			changeToPointIconOnHover(value)
 		elseif value:IsA("Frame") then
 			if (value:GetAttribute("Dark")) then
 				changeDarkerBackGroundColor(value)
@@ -85,10 +107,126 @@ local function changeColor()
 	end
 end
 
+local function addModelToFrame(frame: ViewportFrame, model: Model)
+	if frame:FindFirstChild("Model") then
+		frame.Model:Destroy()
+	end
+	
+	local modelSize = model:GetExtentsSize()
+	local rootPart = model:FindFirstChild("TileRootPart")
+	if rootPart == nil then
+		rootPart = Instance.new("Part")
+		rootPart.CanCollide = false
+		rootPart.Transparency = 1
+		rootPart.Name = "TileRootPart"
+	end
 
+	rootPart.Position = model:GetBoundingBox().Position - Vector3.new(0,modelSize.Y/2,0)
+	rootPart.Size = Vector3.one
+	rootPart.Parent = model
+
+	model.PrimaryPart = rootPart
+	model:PivotTo(CFrame.new())
+	
+	frame.CurrentCamera.CFrame = CFrame.new(Vector3.new(0,modelSize.X,0)) * frame.CurrentCamera.CFrame.Rotation
+	model.Parent = frame
+end
+
+local function validTileConfig()
+	if TileNameBox.Text == "" then
+		return false
+	end
+	if TileModelFrame:FindFirstChild("Model") == nil then
+		return false
+	end
+	for _, connection in pairs(TileConectionsFrame:GetChildren()) do
+		if not connection:IsA("Frame") then continue end
+		if connection.ConnectionType.Text == "" then
+			return false
+		end
+	end
+	return true
+
+end
+
+local function openConnectionsFrame(tileFrame: Frame, tile: Folder)
+	tileConnectionsIsOpen = true
+	tileConnected = tile
+
+	local connections = tile:GetAttributes()
+	for connectionType, connectionValue in pairs(connections) do
+		TileConnectionsList[connectionType].ConnectionType.Text = connectionValue	
+	end
+	TileConnectionsFrame:TweenSize(UDim2.fromScale(.5,.5),Enum.EasingDirection.InOut,Enum.EasingStyle.Quad,.5,true)
+
+	TileConnectionsFrame.Visible = true
+end
+
+local function closeConnectionsFrame()
+	tileConnectionsIsOpen = false
+	tileConnected = nil
+	
+	TileConnectionsFrame:TweenSize(UDim2.new(),Enum.EasingDirection.InOut,Enum.EasingStyle.Quad,.5,true)
+
+	TileConnectionsFrame.Visible = false
+end
+
+local function addTileToList(Tile: Folder) 
+	local TileFrame = TemplateTile:Clone()
+	TileFrame.ModelFrame.CurrentCamera = TileFrame.ModelFrame.Camera
+
+	addModelToFrame(TileFrame.ModelFrame, Tile.Model:Clone())
+
+	TileFrame.Title.Text = Tile.Name
+	local TileConnection = Instance.new("ObjectValue")
+	TileConnection.Value = Tile
+	TileConnection.Parent = TileFrame
+
+	changeToPointIconOnHover(TileFrame.Connections)
+	changeToPointIconOnHover(TileFrame.RemoveButton)
+
+	TileFrame.Connections.MouseButton1Click:Connect(function()
+		if tileConnectionsIsOpen then return end
+		openConnectionsFrame(TileFrame, Tile)
+	end)
+	TileFrame.RemoveButton.MouseButton1Click:Connect(function()
+		Tile:Destroy()
+		TileFrame:Destroy()
+	end)
+	TileFrame.Parent = TilesList
+	TileFrame.Visible = true
+end
 
 local function addTile()
+	local Tiles = ServerStorage:FindFirstChild("Tiles")
+	if Tiles == nil then 
+		Tiles = Instance.new("Folder")
+		Tiles.Name = "Tiles"
+		Tiles.Parent = ServerStorage
+	end
 	
+	local Tile = Instance.new("Folder")
+	Tile.Name = TileNameBox.Text
+	for _, connection in pairs(TileConectionsFrame:GetChildren()) do
+		if not connection:IsA("Frame") then continue end
+		Tile:SetAttribute(connection.Name,connection.ConnectionType.Text)
+	end
+	TileModelFrame.Model.Parent = Tile
+	Tile.Parent = Tiles
+	addTileToList(Tile)
+end
+
+local function resetTileAdderFrame()
+	if TileModelFrame:FindFirstChild("Model") then
+		TileModelFrame.Model:Destroy()
+	end
+	TileModelButton.Text = "Model Preview"
+	modelSelectionMode = false
+	TileNameBox.Text = "Tile"
+	for _, connection in pairs(TileConectionsFrame:GetChildren()) do
+		if not connection:IsA("Frame") then continue end
+		connection.ConnectionType.Text = "0"
+	end
 end
 
 local function openTileAdder()
@@ -97,6 +235,7 @@ local function openTileAdder()
 end
 
 local function closeTileAdder()
+	resetTileAdderFrame()
 	TileAdderFrame:TweenPosition(UDim2.fromScale(.5,-1),Enum.EasingDirection.In,Enum.EasingStyle.Quad,.5,false)
 end
 
@@ -108,26 +247,44 @@ TileAdderButton.MouseButton1Click:Connect(function()
 	end
 end)
 
-AddTileButton.MouseButton1Click:Connect(function()
+TileAdderCloseButton.MouseButton1Click:Connect(function()
 	if tileAdderIsOpen then 
 		tileAdderIsOpen = false
 		closeTileAdder() 
 	end
 end)
 
+AddTileButton.MouseButton1Click:Connect(function()
+	if tileAdderIsOpen then
+		if validTileConfig() then
+			addTile()
+			tileAdderIsOpen = false
+			closeTileAdder() 
+		else
+			AddTileButton.BackgroundColor3 = Color3.fromRGB(164, 0, 0)
+			task.wait(.5)
+			changeButtonColor(AddTileButton)
+		end
+	end
+end)
+
 function selectModel(selected)
 	if selected and selected[1] and selected[1]:IsA("Model") then
 		modelSelectionMode = false
-		local model = selected[1]:Clone()
+		
+
+		local model: Model = selected[1]:Clone()
+		
+		addModelToFrame(TileModelFrame, model)
+
 		model.Name = "Model"
-		model.Parent = TileModelFrame
 		TileModelButton.Text = ""
+		return true
 	end
+	return false
 end
 
 TileModelButton.MouseButton1Click:Connect(function()
-	local Selected = Selection:Get()
-	if not selectModel(Selection:Get()) then end
 	if modelSelectionMode then
 		modelSelectionMode = false
 		if TileModelFrame:FindFirstChild("Model") then
@@ -140,6 +297,7 @@ TileModelButton.MouseButton1Click:Connect(function()
 		modelSelectionMode = true
 	end
 
+	selectModel(Selection:Get())
 end)
 
 Selection.SelectionChanged:Connect(function()
@@ -149,11 +307,30 @@ Selection.SelectionChanged:Connect(function()
 	
 end)
 
+TileConnectionsCloseButton.MouseButton1Click:Connect(function()
+	closeConnectionsFrame()
+end)
+
+for _, connection in pairs(TileConnectionsList:GetChildren()) do
+	if not connection:IsA("Frame") then continue end
+
+	connection.ConnectionType.Changed:Connect(function()
+		if tileConnected ~= nil and connection.ConnectionType.Text ~= "" then
+			tileConnected:SetAttribute(connection.Name, connection.ConnectionType.Text)
+		end
+	end)
+end
+
 widget.Title = "WFC Plugin"
 
 changeColor()
 MainFrame.Parent = widget
 
+if ServerStorage:FindFirstChild("Tiles") then
+	for _, tile in pairs(ServerStorage.Tiles:GetChildren()) do
+		addTileToList(tile)
+	end
+end
 
 Studio.Changed:Connect(function()
 	changeColor()
